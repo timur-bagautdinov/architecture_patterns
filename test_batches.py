70f5a955d6273aaf8e5d3ae3edca09a4a17aea2f
Timur Bagautdinov <timur.bagautdinov@gmail.com>
2026-05-24 16:05:46 +0300

    add new test

--- test_batches.py
+++ test_batches.py
@@ -49,3 +49,11 @@ def test_can_only_deallocate_allocated_lines() -> None:
     batch.deallocate(unallocated_line)
 
     assert batch.available_quantity == 20
+
+
+def test_allocation_is_idempotent() -> None:
+    batch, line = make_batch_and_line("ANGULAR-DESK", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
