d4368ca454d5498fb347c8ee410f40583d839a4b
Timur Bagautdinov <timur.bagautdinov@gmail.com>
2026-05-24 15:58:23 +0300

    store allocations

--- model.py
+++ model.py
@@ -11,14 +11,28 @@ class OrderLine:
 
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int,
-                 eta: Optional[date]):
+                 eta: Optional[date]) -> None:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
     
     def allocate(self, line: OrderLine) -> None:
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+    
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+    
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
--- test_batches.py
+++ test_batches.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from model import Batch, OrderLine
 
+
 def make_batch_and_line(sku: str, batch_qty: int, line_qty: int) -> tuple[Batch, OrderLine]:
     return (Batch("batch-001", sku, batch_qty, eta=date.today()),
             OrderLine("order-123", sku, line_qty))
@@ -24,13 +25,13 @@ def test_can_allocate_if_available_greater_than_required() -> None:
 
 
 def test_cannot_allocate_if_available_smaller_than_required() -> None:
-    small_batch, large_line = make_batch_and_line("ELEGANT_LAMP", 2, 20)
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
 
     assert small_batch.can_allocate(large_line) is False
 
 
 def test_can_allocate_if_available_equal_to_required() -> None:
-    batch, line = make_batch_and_line("ELEGANT_LAMP", 2, 2)
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
 
     assert batch.can_allocate(line)
 
@@ -40,3 +41,11 @@ def test_cannot_allocate_if_skus_do_not_match() -> None:
     different_sku_line = OrderLine("order-123", "TOASTER", 10)
 
     assert batch.can_allocate(different_sku_line) is False
+
+
+def test_can_only_deallocate_allocated_lines() -> None:
+    batch, unallocated_line = make_batch_and_line("TRINKET", 20, 2)
+
+    batch.deallocate(unallocated_line)
+
+    assert batch.available_quantity == 20
